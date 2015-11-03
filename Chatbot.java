1ac0e654c948fab1031f0987629dabd8dca479fb
marc <manzomarc@gmail.com>
2015-11-03 09:44:36 -0700

    i did something!!!

--- Chatbot.java
+++ Chatbot.java
@@ -3,122 +3,139 @@ package chat.model;
 import java.util.ArrayList;
 
 /**
- * Base version of the 2015 Chatbot class. Only stub methods are provided. Students will complete methods as part
- * of the project.
+ * Base version of the 2015 Chatbot class. Only stub methods are provided.
+ * Students will complete methods as part of the project.
+ * 
  * @author Marc Manzo
  * @version 1.0 10/21/15
  */
-public class Chatbot
-{
+public class Chatbot {
 	private ArrayList<String> memesList;
 	private ArrayList<String> politicalTopicList;
 	private String userName;
 	private String content;
-	
+
 	/**
 	 * Creates an instance of the Chatbot with the supplied username.
-	 * @param userName The username for the chatbot.
+	 * 
+	 * @param userName
+	 *            The username for the chatbot.
 	 */
-	public Chatbot(String userName)
-	{
-		
+	public Chatbot(String userName) {
+
 	}
-	
-	private void buildMemesList()
-	{
-		
+
+	private void buildMemesList() {
+
 	}
-	
-	private void buildPoliticalTopicsList()
-	{
-		
+
+	private void buildPoliticalTopicsList() {
+
 	}
-	
+
 	/**
-	 * Checks the length of the supplied string. Returns false if the supplied String is empty or null,
-	 * otherwise returns true. 
+	 * Checks the length of the supplied string. Returns false if the supplied
+	 * String is empty or null, otherwise returns true.
+	 * 
 	 * @param currentInput
 	 * @return A true or false based on the length of the supplied String.
 	 */
-	public boolean lengthChecker(String currentInput)
+
+	public boolean lengthChecker(String currentInput) 
 	{
-		return false;
+		boolean hasLength = false;
+		
+			if(currentInput != null)
+
+		if (currentInput.length() <= 1)
+		{
+			hasLength = true; 
+		}
+
+			hasLength = true;
+		return hasLength;
 	}
-	
+
 	/**
-	 * Checks if the supplied String matches the content area for this Chatbot instance.
-	 * @param currentInput The supplied String to be checked.
+	 * Checks if the supplied String matches the content area for this Chatbot
+	 * instance.
+	 * 
+	 * @param currentInput
+	 *            The supplied String to be checked.
 	 * @return Whether it matches the content area.
 	 */
-	public boolean contentChecker(String currentInput)
-	{
+	public boolean contentChecker(String currentInput) {
 		return false;
 	}
-	
+
 	/**
-	 * Checks if supplied String matches ANY of the topics in the politicalTopicsList. Returns
-	 * true if it does find a match and false if it does not match.
-	 * @param currentInput The supplied String to be checked.
+	 * Checks if supplied String matches ANY of the topics in the
+	 * politicalTopicsList. Returns true if it does find a match and false if it
+	 * does not match.
+	 * 
+	 * @param currentInput
+	 *            The supplied String to be checked.
 	 * @return Whether the String is contained in the ArrayList.
 	 */
-	public boolean politicalTopicChecker(String currentInput)
-	{
+	public boolean politicalTopicChecker(String currentInput) {
 		return false;
 	}
-	
-	
+
 	/**
-	 * Checks to see that the supplied String value is in the current memesList variable.
-	 * @param currentInput The supplied String to be checked.
-	 * @return Whether the supplied String is a recognized meme.
+	 * Checks to see that the supplied String value is in the current memesList
+	 * variable.
+	 * 
+	 * @param currentInput
+	 *            The supplied String to be checked.
+	 * @return Whether the supplied String is a recognized memo.
 	 */
-	public boolean memeChecker(String currentInput)
-	{
+	public boolean memeChecker(String currentInput) {
 		return false;
 	}
-	
+
 	/**
 	 * Returns the username of this Chatbot instance.
+	 * 
 	 * @return The username of the Chatbot.
 	 */
-	public String getUserName()
-	{
+	public String getUserName() {
 		return null;
 	}
-	
+
 	/**
 	 * Returns the content area for this Chatbot instance.
+	 * 
 	 * @return The content area for this Chatbot instance.
 	 */
-	public String getContent()
-	{
+	public String getContent() {
 		return null;
 	}
-	
+
 	/**
 	 * Getter method for the memesList object.
+	 * 
 	 * @return The reference to the meme list.
 	 */
-	public ArrayList<String> getMemesList()
-	{
+	public ArrayList<String> getMemesList() {
 		return null;
 	}
-	
+
 	/**
 	 * Getter method for the politicalTopicList object.
+	 * 
 	 * @return The reference to the political topic list.
 	 */
-	public ArrayList<String> getPoliticalTopicList()
-	{
+	public ArrayList<String> getPoliticalTopicList() {
 		return null;
 	}
-	
+
 	/**
 	 * Updates the content area for this Chatbot instance.
-	 * @param content The updated value for the content area.
+	 * 
+	 * @param content
+	 *            The updated value for the content area.
 	 */
-	public void setContent(String content)
-	{
-		
+	public void setContent(String content) {
+
 	}
 }
